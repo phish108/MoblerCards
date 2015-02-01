ad70f16a4425790b48e0a250fb9ccf9cd2a4b071
Christian Glahn <cpglahn@gmail.com>
2015-02-02 00:27:31 +0100

    proper update logic

--- www/js/views/loginView.js
+++ www/js/views/loginView.js
@@ -2,7 +2,7 @@
 
 /**	THIS COMMENT MUST NOT BE REMOVED
 Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file 
+or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
 regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
@@ -16,7 +16,7 @@ software distributed under the License is distributed on an
 "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
-under the License.	
+under the License.
 */
 
 /**
@@ -48,7 +48,7 @@ function LoginView() {
     this.active = false;
     this.fixedRemoved = false;
 
-    /** 
+    /**
      * It is triggered when an online connection is detected.
      * @event errormessagehide
      * @param: a function that hides the error message from login view
@@ -56,6 +56,14 @@ function LoginView() {
     $(document).bind("errormessagehide", function () {
         console.log(" hide error message loaded ");
         self.hideErrorMessage();
+    });
+
+    $(document).bind("DEVICE_ONLINE", function () {
+        self.hideErrorMessage();
+    });
+
+    $(document).bind("DEVICE_OFFLINE", function () {
+        self.showErrorMessage(jQuery.i18n.prop('msg_network_message'));
     });
 }
 
@@ -68,14 +76,32 @@ function LoginView() {
  **/
 LoginView.prototype.prepare = function () {
     console.log("loginView: open sesame");
-    // hide unnecessary errors and warnings 
+    // hide unnecessary errors and warnings
     this.hideErrorMessage();
     this.hideWarningMessage();
     this.hideDeactivateMessage();
     $("#selectLMS").removeClass("gradientSelected");
-    this.showForm();
     this.active = true;
     this.app.models.featured.loadFeaturedCourseFromServer();
+};
+
+LoginView.prototype.update = function () {
+    var activeLMS = {};
+    this.app.models.lms.getActiveLMS(function (data) {
+        activeLMS = data;
+    });
+
+    $("#loginimg").attr("src", activeLMS.logofile);
+
+    // TODO: TRANSFORM STRING TO i18n.prop
+    $("#loginlmslabel").text(activeLMS.name || "select an LMS");
+
+    this.hideErrorMessage();
+    this.hideDeactivateMessage();
+
+    if (this.app.models.connection.isOffline()) {
+        this.showErrorMessage(jQuery.i18n.prop('msg_network_message'));
+    }
 };
 
 /**
@@ -103,10 +129,10 @@ LoginView.prototype.tap = function (event) {
 
     if (id === "loginbutton") {
         this.clickLoginButton();
-    } 
+    }
     else if (id === "loginclose") {
         this.clickCloseLoginButton();
-    } 
+    }
     else if (id === "loginlmslabel") {
         $("#selectLMS").removeClass("textShadow");
         $("#selectLMS").addClass("gradientSelected");
@@ -175,10 +201,10 @@ LoginView.prototype.clickLoginButton = function () {
 
             self.showWarningMessage(jQuery.i18n.prop('msg_warning_message'));
             this.app.models.configuration.login($("#usernameInput").val(), $("#password").val());
-        } 
+        }
     // use else to display an error message that the internet connectivity is lost, or remove the if sanity check (offline)
     // the isOffline seems to work not properly
-    } 
+    }
     else {
         self.showErrorMessage(jQuery.i18n.prop('msg_authentication_message'));
     }
@@ -190,15 +216,7 @@ LoginView.prototype.clickLoginButton = function () {
  * @function showForm
  */
 LoginView.prototype.showForm = function () {
-    $("#loginimg").attr("src", this.app.getActiveLogo());
-    $("#loginlmslabel").text(this.app.getActiveLabel());
 
-    this.hideErrorMessage();
-    this.hideDeactivateMessage();
-    
-    if (this.app.models.connection.isOffline()) {
-        this.showErrorMessage(jQuery.i18n.prop('msg_network_message'));
-    }
 };
 
 /**
@@ -270,15 +288,17 @@ LoginView.prototype.hideDeactivateMessage = function () {
     console.log("hided deactivate message");
 };
 
-/** 
+/**
  * storing the selected LMS  in an array
  * @prototype
  * @function storeSelectedLMS
+ *
+ * The active LMS is defined by the model not the login view
  * */
 LoginView.prototype.storeSelectedLMS = function () {
-    var selectedLMS = $("#loginlmslabel").text();
-    console.log("stored selected lms is" + JSON.stringify(selectedLMS));
-    this.app.models.lms.setSelectedLMS(selectedLMS);
+    //var selectedLMS = $("#loginlmslabel").text();
+    // console.log("stored selected lms is" + JSON.stringify(selectedLMS));
+    // this.app.models.lms.setSelectedLMS(selectedLMS);
 };
 
 /**
@@ -290,8 +310,6 @@ LoginView.prototype.storeSelectedLMS = function () {
 LoginView.prototype.clickCloseLoginButton = function () {
     //set the active server to be the previous server
     var lmsModel = this.app.models.lms;
-    var activeServer = lmsModel.getActiveServer();
 
-    lmsModel.storePreviousServer(activeServer);
     this.app.changeView("landing");
 };
