2819836adc82967586c02f8a11dedac26d2a6fc3
Dijan Daishen <dijandaishen@outlook.com>
2015-02-14 15:42:19 +0100

    [QuestionView] tab event

--- www/js/views/questionView.js
+++ www/js/views/questionView.js
@@ -93,6 +93,7 @@ QuestionView.prototype.tap = function (event) {
     console.log("[QuestionView] tap registered: " + id);
 
     if (id === "questionbutton" ||
+        id === "questionlisttext" ||
         id === "questioncontent") {
         if (this.app.models.answer.answerScore > -1) {
             this.app.changeView("feedback");
