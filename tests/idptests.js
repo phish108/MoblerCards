3c00a72ca92c6cf2c9d1a5ec9a781d579742e5bf
Christian Glahn <cpglahn@gmail.com>
2015-07-26 22:51:18 +0200

    more model test cases

---
+++ tests/idptests.js
@@ -0,0 +1,16 @@
+/*jslint
+  white: true, vars: true, sloppy: true, devel: true, plusplus: true, browser: true
+ */
+
+/*global QUnit, IdentityProvider*/
+
+
+QUnit.test( "ID Provider Tests", function( assert ) {
+    var idp = new IdentityProvider();
+    assert.ok(idp);
+
+    /*
+     * RSD Loading Tests
+     */
+
+});
