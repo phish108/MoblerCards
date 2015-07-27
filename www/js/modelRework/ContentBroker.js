b44fee63b1e517a9f8c83e76cbc38d119eab0e8b
Christian Glahn <cpglahn@gmail.com>
2015-07-27 08:31:06 +0200

    sign the requests with the Access Token

--- www/js/modelRework/ContentBroker.js
+++ www/js/modelRework/ContentBroker.js
@@ -1,23 +1,22 @@
 /*jslint white: true, vars: true, sloppy: true, devel: true, plusplus: true, browser: true, todo: true */
 /*global $, Promise */
 
-/**	THIS COMMENT MUST NOT BE REMOVED
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0  or see LICENSE.txt
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
+/**	THIS COMMENT MUST NOT BE REMOVED  AND REMAIN INTACT
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0  or see LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
 */
 
 /**
@@ -256,6 +255,13 @@ ContentBroker.prototype.fetchQuestionpoolList = function (courseId, lmsId) {
         if (serviceURL) {
             $.ajax({
                 url: serviceURL + "/" + courseId,
+                type: "GET",
+                beforeSend: function (xhr, settings) {
+                    self.idprovider.sessionHeader(xhr,
+                                                  settings.url,
+                                                  settings.type,
+                                                  ["MAC", "Bearer"]);
+                },
                 success: function (datalist) {
                     if (datalist && Array.isArray(datalist)) {
                         resolve(datalist);
@@ -386,7 +392,14 @@ ContentBroker.prototype.fetchCourseList = function (lmsId) {
         var serviceURL = self.idprovider.serviceURL("powertla.content.courselist",lmsId);
         if (serviceURL) {
             $.ajax({
+                "type": "GET",
                 url: serviceURL,
+                beforeSend: function (xhr, settings) {
+                    self.idprovider.sessionHeader(xhr,
+                                                  settings.url,
+                                                  settings.type,
+                                                  ["MAC", "Bearer"]);
+                },
                 success: function (datalist) {
                     if (datalist && Array.isArray(datalist)) {
                         resolve(datalist);
